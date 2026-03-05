3b87aceae67a228f3363e87a9fee0bbd633880e3
pricillaruby <pricillarubysanthiyagu@gmail.com>
2026-03-05 16:24:55 +0530

    Revet "Bank Application was added"
    This reverts commit 3fe1c79056ca916c18e7d9e097b5634e6bdaa3aa.
    
    :
    
    q

--- accounts/src/main/java/bankapp/accounts/repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package bankapp.accounts.repository;
-
-import bankapp.accounts.entity.AccountEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<AccountEntity,Integer> {
-}
